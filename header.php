f2ea138b0bd25ece5cc7fdf28ab52a40eea07f5d
Eliana Prada <darktowerdev@gmail.com>
2017-02-27 17:07:34 -0500

    Inicializacion Landing

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es" xml:lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Vives por ella</title>
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0">
+	<link rel="icon" href="favicon.ico">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/other-styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<header class="header">
+	<div class="header-Container wrap-principal">
+		<div class="header-Container-Block">
+			<a href="" class="block-Button">JOIN US</a>
+		</div>
+		<div class="header-Container-Block">
+			<div class="logo">
+				<img class="balls" src="img/title-balls.svg" alt="">
+				<div class="title">LAS SOCIEDADES DEL BRITÁNICO</div>
+			</div>
+		</div>
+		<div class="header-Container-Block">
+			<div class="lang">
+				<a href="">ES</a>
+				<a href="">EN</a>
+			</div>
+		</div>
+	</div>
+</header>
+	<div class="wrapper">
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php include("header.php");?>
+<section class="mainBack">
+    <div class="wrap-principal mainBack-ImgContainer">
+        <img src="img/back.jpg" alt="">
+    </div>
+</section>
+<?php include("section-menu.php");?>
+<section class="mainConten ">
+    <div class="normalCont wrap-principal">
+        <div class="text text-custom1">
+            <div class="textCont">
+                <p>A muchos alumni les marcaron estas áreas en el Colegio y para muchos aún tienen un sentido. Algunos viven
+                    de ellas, para otros sigue siendo un hobby imprescindible. El Británico les inculcó la pasión por una
+                    de estas áreas, y seguramente, de todos los profesores que han pasado por sus vidas nunca olvidarán a
+                    aquel que los introdujo en una de ellas, en uno de esos vínculos que quizá muchos no han vuelto a pensar
+                    que aún tienencon el Británico.</p>
+            </div>
+            <div class="parallaxImage firstImage">
+                <div class="images" data-0="margin-top:2px;" data-end="margin-top:-100px;">
+                    <img src="img/music-sol.svg" alt="" >
+                </div>
+            </div>
+        </div>
+        <div class="text text-right">
+            <div class="parallaxImage firstImage " >
+                <div class="images p2" data-0="margin-top:2px;" data-end="margin-top:-100px;">
+                    <img src="img/guitar.svg" alt="" >
+                    <img src="img/ruler.svg" alt="" >
+                </div>
+            </div>
+            <div class="textCont">
+                <h3>¿QUÉ QUEREMOS EN LAS SOCIETIES?</h3>
+                <ul>
+                    <li>Seguir contando con todos y cada uno de nuestros antiguos alumnos. </li>
+                    <li>Fortalecer el networking y generar más oportunidades para ellos.</li>
+                    <li>Crear nuevas amistades con personas aﬁnes al Británico.</li>
+                </ul>
+                <p>Queremos multiplicar el impacto del British Council School y recuperar a todos aquellos que han amado el
+                    Colegiopero que con el paso de los años se han desvinculado. A partir de la organización de actividades
+                    o el desarrollode proyectos que fomenten estos intereses comunes. </p>
+                <p>Buscamos también generar sinergias, recaudar fondos para proyectos, organizar actos sociales, actividades
+                    de mentoring o coaching profesional de carácter intergeneracional. Todo cabe en las Sociedades del Británico
+                    siempre que se rija por los valores del Colegio y sirva para fomentar la lealtad, las oportunidades y
+                    las relaciones culturales.</p>
+            </div>
+        </div>
+        <div class="text text-left">
+            <div class="textCont">
+                <h3>¿QUIÉN PUEDE FORMAR PARTE?</h3>
+                <p>Padres, expadres, exalumnos, staff y exstaff podrán ser miembros de todas las sociedades.</p>
+                <p>Las clases, los recreos, aquellos profesores únicos e inolvidables, aquellos compañeros con los que compartir
+                    conﬁdencias, las primeras excursiones, los cumpleaños, los conciertos, el olor de los lápices de colores
+                    o aquel villancico que aún resuena en la cabeza…</p>
+                <p>Nuestros antiguos alumnos son el testimonio vivo del Británico. Con la ﬁnalidad de convertirse en un instrumento
+                    que sirva de cauce para que los antiguos alumnos, padres de los mismos y personal que haya desempeñado
+                    servicios en el Colegio puedan continuar vinculados al mismo y continuar participando de las actividades
+                    y valores del mismo, el British Council School ha promovido la creación de estas Societies para aglutinar
+                    a las citadas personas que tienen intereses comunes en las siguientes actividades.</p>
+            </div>
+            <div class="parallaxImage firstImage">
+                <div class="images p1" data-0="margin-top:2px;" data-end="margin-top:-33px;">
+                    <img src="img/grade-hat.svg" alt="">
+                    <img src="img/telescope.svg" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include("footer.php");?>
